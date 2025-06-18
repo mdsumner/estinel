4e76fe8118b7b634af92ec3c5cddc975afca7dc6
Michael Sumner <mdsumner@gmail.com>
2025-06-18 02:46:08 +0000

    major update

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,41 @@
+#' Plot raster at native resolution
+#'
+#' Determines the current device size and plots the raster centred on its own
+#' middle to plot at native resolution. 
+#'
+#' https://www.hypertidy.org/posts/2024-12-04_plot_native/
+#' @param x as SpatRaster
+#' @param ... passed to plot.window
+#'
+#' @return the input raster, cropped corresponding to the plot made
+#' @export
+
+plot_native <- function(x, asp = 1, ..., dev_px = dev.size("px")) {
+  ex <- attr(x, "gis")$bbox[c(1, 3, 2, 4)]
+  dm <- attr(x, "gis")$dim[1:2]
+  at <- NULL
+  ## take the centre
+  if (is.null(at)) {
+    at <- apply(matrix(ex, 2), 2, mean)
+  }
+  dv <- dev_px
+  scl <- diff(ex)[c(1, 3)] / dm
+  halfx <- dv[1]/2 * scl[1]
+  halfy <- dv[2]/2 * scl[2]
+  cropex <- c(at[1] - halfx, at[1] + halfx, at[2] - halfy, at[2] + halfy)
+  #x <- terra::crop(x, terra::ext(cropex), extend = TRUE)
+  #add <- FALSE
+  #if (terra::nlyr(x) >= 3) terra::plotRGB(x, add = add) else plot(x, ..., add = add)
+  plot.new()
+  plot.window(cropex[1:2], cropex[3:4], asp = asp, ...)
+  ximage::ximage(x, add = TRUE)
+  invisible(NULL)
+}
+
+
+
+
+
 set_gdal_envs <- function() {
   Sys.setenv("GDAL_HTTP_MAX_RETRY" = "4")
   Sys.setenv("GDAL_DISABLE_READDIR_ON_OPEN" = "EMPTY_DIR")
@@ -96,7 +134,8 @@ process_stac_table <- function(js, llex, location, crs, extent) {
 
 
 ## build the image
-build_cloud <- function(assets, res = 10) {
+build_cloud <- function(assets, res = 10, div = NULL) {
+  out <- try({
   tmpdir <- "/perm_storage/home/data/_targets_sentineltifs"
  # dir.create(tmpdir)
   tf <- tempfile(fileext = ".tif", tmpdir = tmpdir)
@@ -104,11 +143,16 @@ build_cloud <- function(assets, res = 10) {
   exnames <- c("xmin", "xmax", "ymin", "ymax")
   ## every group of rows has a single extent
   ex <- unlist(assets[1L, exnames], use.names = FALSE)
+  ## this only works for our crafted extents
+  if (!is.null(div)) ex <- ex/div
   cloud <- sprintf("/vsicurl/%s", assets$scl)
   vapour::gdal_raster_dsn(cloud, target_crs= assets$crs[1], target_res = res, target_ext = ex, out_dsn = tf)
+  })
+  if (inherits(out, "try-error")) return(NULL)
+  out
 }
 
-warp_and_read <- function(dsn, target_ext = NULL, target_crs = NULL, target_res = NULL, target_dim = NULL) {
+warp_to_dsn <- function(dsn, target_ext = NULL, target_crs = NULL, target_res = NULL, target_dim = NULL, resample = "near") {
   set_gdal_envs()
   dsn <- sprintf("/vsicurl/%s", dsn)
   args <- character()
@@ -117,6 +161,7 @@ warp_and_read <- function(dsn, target_ext = NULL, target_crs = NULL, target_res 
     t_srs <- target_crs
   }
   if (!is.null(target_ext)) {
+    
     args <- c(args, "-te", target_ext[c(1, 3, 2, 4)])
   }
   if (!is.null(target_res)) {
@@ -128,12 +173,12 @@ warp_and_read <- function(dsn, target_ext = NULL, target_crs = NULL, target_res 
     target_dim <- rep(target_dim, length.out = 2L)
     args <- c(args, "-ts", target_dim)
   }
- #t_srs <- gdalraster::srs_to_wkt(t_srs)
+  #t_srs <- gdalraster::srs_to_wkt(t_srs)
   chk <- gdalraster::warp(dsn, tf <- tempfile(fileext = ".tif", tmpdir = "/vsimem"), t_srs = t_srs, cl_arg = args, quiet = TRUE)
-  x <- gdalraster::read_ds(new(gdalraster::GDALRaster, tf))
-  x
+  #x <- gdalraster::read_ds(new(gdalraster::GDALRaster, tf))
+  tf
 }
-build_image <- function(assets, res) {
+build_image_dsn <- function(assets, res, resample = "near") {
   set_gdal_envs()
   crs <- assets$crs[1]
   
@@ -145,31 +190,115 @@ build_image <- function(assets, res) {
   bandnames <- c("red", "green", "blue")
   ## didn't like use of gdalraster::srs_to_wkt here ??
   for (i in seq_along(bandnames)) {
-    bands[[i]] <- warp_and_read(assets[[bandnames[i]]], target_crs = crs, target_res = res, target_ext = ex)
+    bands[[i]] <- warp_to_dsn(assets[[bandnames[i]]], target_crs = crs, target_res = res, target_ext = ex, resample = resample)
   }
-  ## bit sneaky here to leverage plotraster "gis" idiom while also using dim, but ha sue me
-  out <- do.call(cbind, lapply(bands, mtrx))
-  gis <- attr(bands[[1]], "gis")
-  gis$dim <- c(gis$dim[1:2], ncol(out))
-  gis$datatype <- rep(gis$datatype, length.out = ncol(out))
-  attr(out, "gis") <- gis
-  out
+  vrt <- vapour::buildvrt(unlist(bands))
+  location <- assets$location[1L]
+  outfile <- sprintf("/perm_storage/home/data/_targets_locationtifs/sentinel-2-c1-l2a/%s/%s_%s.tif", format(assets$solarday[1], "%Y/%m/%d"), location, format(assets$solarday[1]))
+  fs::dir_create(dirname(outfile))
+  gdalraster::translate(vrt, outfile, cl_arg = c("-co", "COMPRESS=DEFLATE", "-co", "TILED=NO"))
+  
+  ## when making this s3 we can drop the substitution
+  # jsontext <- gsub("href\": \"idea-sentinel2-locations", "href\": \"https://projects.pawsey.org.au/idea-sentinel2-locations", json$dumps(stac$create_stac_item(outfile, date, collection = "idea-sentinel2-locations", with_proj = TRUE, with_raster = FALSE)$to_dict()))
+  # jsonfile <- sprintf("%s-stac-item.json", outfile)
+  # writeLines(jsontext, jsonfile)
+  # 
+  # 
+  # outfile2 <- sprintf("/perm_storage/home/data/_targets_locationtifs/sentinel-2-c1-l2a/%s/%s_%s_stretch.tif", format(assets$solarday[1], "%Y/%m/%d"), location, format(assets$solarday[1]))
+  # out2 <- terra::stretch(log(terra::rast(outfile)))
+  # terra::writeRaster(out2, outfile2, datatype = "INT1U", gdal = c("COMPRESS=DEFLATE", "TILED=NO"), overwrite = TRUE)
+  # 
+  # date <- as.POSIXct(stringr::str_extract(basename(outfile2), "[0-9]{4}\\-[0-9]{2}\\-[0-9]{2}"), tz = "UTC")
+  # jsontext <- gsub("href\": \"idea-sentinel2-locations", "href\": \"https://projects.pawsey.org.au/idea-sentinel2-locations", json$dumps(stac$create_stac_item(outfile2, date, collection = "idea-sentinel2-locations", with_proj = TRUE, with_raster = FALSE)$to_dict()))
+  # jsonfile <- sprintf("%s-stac-item.json", outfile2)
+  # writeLines(jsontext, jsonfile)
+  # 
+
+  
+  outfile
+
 }
+
+
+
+# warp_and_read <- function(dsn, target_ext = NULL, target_crs = NULL, target_res = NULL, target_dim = NULL, resample = "near") {
+#   set_gdal_envs()
+#   dsn <- sprintf("/vsicurl/%s", dsn)
+#   args <- character()
+#   t_srs <- ""
+#   if (!is.null(target_crs)) {
+#     t_srs <- target_crs
+#   }
+#   if (!is.null(target_ext)) {
+# 
+#     args <- c(args, "-te", target_ext[c(1, 3, 2, 4)])
+#   }
+#   if (!is.null(target_res)) {
+#     target_res <- rep(target_res, length.out = 2L)
+#     
+#     args <- c(args, "-tr", target_res)
+#   }
+#   if (!is.null(target_dim)) {
+#     target_dim <- rep(target_dim, length.out = 2L)
+#     args <- c(args, "-ts", target_dim)
+#   }
+#  #t_srs <- gdalraster::srs_to_wkt(t_srs)
+#   chk <- gdalraster::warp(dsn, tf <- tempfile(fileext = ".tif", tmpdir = "/vsimem"), t_srs = t_srs, cl_arg = args, quiet = TRUE)
+#   x <- gdalraster::read_ds(new(gdalraster::GDALRaster, tf))
+#   x
+# }
+# build_image <- function(assets, res, resample = "near") {
+#   set_gdal_envs()
+#   crs <- assets$crs[1]
+#   
+#   exnames <- c("xmin", "xmax", "ymin", "ymax")
+#   ## every group of rows has a single extent
+#   ex <- unlist(assets[1L, exnames], use.names = FALSE)
+#   
+#   bands <- vector("list", 3)
+#   bandnames <- c("red", "green", "blue")
+#   ## didn't like use of gdalraster::srs_to_wkt here ??
+#   for (i in seq_along(bandnames)) {
+#     bands[[i]] <- warp_and_read(assets[[bandnames[i]]], target_crs = crs, target_res = res, target_ext = ex, resample = resample)
+#   }
+#   ## bit sneaky here to leverage plotraster "gis" idiom while also using dim, but ha sue me
+#   #out <- do.call(cbind, lapply(bands, mtrx))
+#   gis <- attr(bands[[1]], "gis")
+#   
+#   out <- unlist(bands, use.names = TRUE)
+#   gis$dim <- c(gis$dim[1:2],length(bands))
+#   gis$datatype <- rep(gis$datatype, length.out = length(bands))
+#   attr(out, "gis") <- gis
+#   out
+# }
 mtrx <- function(x) {
-  as.vector(matrix(x, attr(x, "gis")$dim[2L], byrow = TRUE))
+  as.vector(x)# matrix(x, attr(x, "gis")$dim[2L], byrow = TRUE))
 }
 read_dsn <- function(x) {
-  ds <- new(gdalraster::GDALRaster, x)
+  if (is.null(x)) return(NULL)
+  ds <- new(gdalraster::GDALRaster, x[[1]])
   on.exit(ds$close(), add = TRUE)
   gdalraster::read_ds(ds)
 }
 filter_fun <- function(.x) {
+  if (is.null(.x)) return(FALSE)
   .x <- unlist(.x, use.names = F)
   if (all(is.na(.x))) return(TRUE)
-  mean(.x %in%  c(0, 1, 2, 3, 8, 9, 10), na.rm = TRUE) > .4
+  ## 0, 1, 2, 3, 
+  mean(.x %in%  c(1, 3, 8, 9, 10), na.rm = TRUE) < .4
 }
-qm <- function(x, target_dim = c(1024, 0), target_crs = NULL, target_ext = NULL,target_res = NULL,...) {
-  out <- vapour::gdal_raster_nara(sprintf("/vsicurl/%s", x), target_dim = target_dim, target_res = target_res, target_crs = target_crs, target_ext = target_ext)
+qm <- function(x, target_dim = c(1024, 0), target_crs = NULL, target_ext = NULL,target_res = NULL,..., scale = F, band_output_type = "raw") {
+  dsn <- sprintf("/vsicurl/%s", x)
+  if (scale) {
+    dsn <- sprintf("vrt://%s?scale=true", dsn)
+   # band_output_type <- "double"
+    out <- vapour::gdal_raster_data(dsn, target_dim = target_dim, target_res = target_res, target_crs = target_crs, target_ext = target_ext)
+    
+  } else {
+    out <- vapour::gdal_raster_nara(dsn, target_dim = target_dim, target_res = target_res, target_crs = target_crs, target_ext = target_ext, 
+                                    band_output_type = band_output_type)
+    
+  }
   ximage::ximage(out, ...)
   invisible(out)
 }
--- _targets.R
+++ _targets.R
@@ -19,22 +19,34 @@ tar_option_set(
 tar_source()
 # tar_source("other_functions.R") # Source other scripts as needed.
 
-
+cleanup_table <- function() {
+  x <- readxl::read_excel("Emperor penguin colony locations_all_2024.xlsx", skip = 2) |> 
+     dplyr::rename(location = colony, lon = long) |> dplyr::select(-date)
+  stp <- unlist(gregexpr("[\\[\\(]", x$location)) -1
+  stp[stp < 0] <- nchar(x$location[stp < 0])
+  x$location <- substr(x$location, 1, stp)
+  x$location <- trimws(x$location)
+  x$location <- gsub("\\s+", "_", x$location, perl = TRUE)
+  x$location <- gsub("é", "e", x$location)
+  x
+}
 
 # Replace the target list below with your own:
 list(
-  tar_target(tabl, rbind(data.frame(location = "Davis", lon = c(77 + 58/60 + 3/3600), lat = -(68 + 34/60 + 36/3600)), 
+  tar_target(tabl, rbind(
+    data.frame(location = "Davis", lon = c(77 + 58/60 + 3/3600), lat = -(68 + 34/60 + 36/3600)), 
                                  data.frame(location = "Casey", 
-                                            lon = cbind(110 + 31/60 + 36/3600), lat =  -(66 + 16/60 + 57/3600)), 
+                                           lon = cbind(110 + 31/60 + 36/3600), lat =  -(66 + 16/60 + 57/3600)), 
                                  data.frame(location = "Heard", lon = 73 + 30/60 + 30/3600, lat = -(53 + 0 + 0/3600)),
+                         data.frame(location = "Heard", lon = 73.38681, lat = -53.024348),
                                  data.frame(location = "Mawson", lon = 62 + 52/60 + 27/3600, lat = -(67 + 36/60 + 12/3600)),
-                                 data.frame(location = "Macquarie", lon = 158.93835, lat = -54.49871),
-                                 readxl::read_excel("Emperor penguin colony locations_all_2024.xlsx", skip = 2) |> 
-                                   dplyr::rename(location = colony, lon = long) |> dplyr::select(-date))
-  ),
+                                 data.frame(location = "Macquarie", lon = 158.93835, lat = -54.49871)
+                                , cleanup_table() 
+  )),
   
   tar_target(radiusy, 3000), tar_target(radiusx, radiusy),
-  tar_target(daterange, stac_date()),
+  #tar_target(daterange, stac_date(365)),
+  tar_target(daterange, format(as.POSIXct(c(as.POSIXct("2015-06-23 00:00:00", tz = "UTC"), Sys.time())))),
   tar_target(lon, tabl$lon), tar_target(lat, tabl$lat), tar_target(location, tabl$location),
   tar_target(crs, mk_crs(lon, lat), pattern = map(lon, lat), iteration = "vector"),
   ## note that mkextent and unproj are vectorized over matrix rows
@@ -51,27 +63,35 @@ list(
   tar_target(stac_json, getstac_json(qtable), pattern = map(qtable), iteration = "list"),
   tar_target(stac_tables, process_stac_table(stac_json, ll_extent, location, crs, extent), pattern = map(stac_json, ll_extent, location, crs, extent), iteration = "list"), 
   tar_target(images_table, dplyr::bind_rows(stac_tables) |> dplyr::group_by(location, solarday) |> tar_group(), iteration = "group"), 
-  tar_target(images_table0, dplyr::filter(images_table, location == "Amundsen Bay") |> dplyr::group_by(location, solarday) |> tar_group(), iteration = "group")
-,  tar_target(cloud_tifs, build_cloud(images_table0, res = 10), pattern = map(images_table0),  iteration = "list"),
+ # tar_target(images_table0, dplyr::filter(images_table, location %in%  c("Heard", "Macquarie")) |> dplyr::group_by(location, solarday) |> tar_group(), iteration = "group"),  
+  tar_target(cloud_tifs, build_cloud(images_table, res = 10, div = 2), pattern = map(images_table),  iteration = "list"),
   ## [[1]] because output of read_dsn() is a list (each has its own attributes, which you can't do on atomic vector)
-tar_target(cloud_filter, filter_fun(read_dsn(cloud_tifs[[1]])), pattern = map(cloud_tifs), iteration = "vector")
-, tar_target(filter_table, images_table0 |> dplyr::filter(tar_group %in% which(cloud_filter)) |> dplyr::group_by(location, solarday) |> tar_group(), iteration = "group")
-,tar_target(images, build_image(images_table0 , res = 10), pattern = map(images_table0), iteration = "list")
+ tar_target(cloud_filter, filter_fun(read_dsn(cloud_tifs)), pattern = map(cloud_tifs), iteration = "vector"), 
+ tar_target(filter_table, 
+             images_table |> dplyr::filter(tar_group %in% which(cloud_filter)) |> 
+               dplyr::group_by(location, solarday) |> tar_group(), 
+             iteration = "group"),
+
+#tar_target(images, build_image(images_table0 , res = 10), pattern = map(images_table0), iteration = "list")
+## finally, filter these by var(iance) there's a bunch that are NA
+tar_target(images, build_image_dsn(filter_table , res = 10), pattern = map(filter_table), iteration = "list")
 )
 
+# library(furrr)
+# options(parallelly.fork.enable = TRUE, future.rng.onMisuse = "ignore")
+# library(furrr); plan(multicore)
+# b <- furrr::future_map_lgl(seq_len(lengths(tar_meta(images)$children)), \(.x) is.na(var(tar_read("images", branches = .x)[[1]], na.rm  = T)))
+# plan(sequential)
+# idx <- which(!b)
+# 
+# bf <- 250
+#imtab <- distinct(tar_read(filter_table), tar_group, location, solarday) |> arrange(tar_group) 
+# #for (i in seq_len(nrow(imtab))) {
+# for (i in 18:nrow(imtab)) {
+#   ximage(log(tar_read(images, branches = imtab$tar_group[i])[[1]]), asp = 1);
+#   title(sprintf("%s: %s", imtab$location[i], imtab$solarday[i])); #rect(-bf, -bf, bf, bf, lty = 2, border = "hotpink");
+#   scan("", 1)
+# }
 
-if (FALSE) {
-  tab <- cbind(1:20, scales::rescale(t(col2rgb(hcl.colors(20)))))
-  xs <- 150
-  ys <- 150
-  tar_load(images)
-  tar_load(cloud_tifs)
-par(mfrow = c(12, 6), mar = rep(0, 4))
-for (i in 1:24 ) {
-gdalraster::plot_raster(images[[i]], xsize = xs, ysize = ys)
-gdalraster::plot_raster(new(gdalraster::GDALRaster, cloud_tifs[[i]][[1]]), col_tbl = tab, xsize = xs, ysize = ys)
 
-}
-
-}
-
+# 
